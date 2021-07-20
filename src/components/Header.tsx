50463bb11c5eb138b3c7435514b828803b7d1b34
seoyoon04 <sykim8483@naver.com>
2021-07-20 23:35:03 +0900

    first commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components"
-import { isPropertySignature } from "typescript";
 
 const HeaderWrapper = styled.div<{height: number}>`
     width: 100vw;
